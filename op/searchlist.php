560cb38101583e541a298a33632e33a51a817088
Júzer <Jzer@192.168.0.192>
2020-10-28 09:09:25 +0100

    advanced.css image-wrapper módosítása portrait-ra searchlist.php grid system átírása 2-3-4 oszlopban jelennek meg a termékek

--- op/searchlist.php
+++ op/searchlist.php
@@ -644,41 +644,44 @@ $total_items = count($json);
 $total_pages = ceil($total_items / $limit);
 $final = array_splice($json, $offset, $limit); // splice them according to offset and limit
 
+echo '<div class="container">
+        <div class="row px-1">';
+
 for ($i=0; $i<$limit; $i++) {
     $item= $final[$i]['advertisement'];
-    echo '  <div class="container">
-                <div class="row">
-                    <div class="col-sm">
-                        <p class="dropdown-divider"></p>
-    			    </div>
-    		    </div>
-        		<div class="row">
-            		<div class="col-sm-5">
-                		<div class="levanderBlushLighter rounded mb-1">
-                    		<a href="../includes/product.php?itemid=' . $item['AdvertisementId'];
-        if(isset($_SESSION['login']))
-            echo '&login=1';
-            echo '" class="image-wrapper">';
-            echo '<img class="img-fluid rounded image" src="' . '/uploadedImages/babacipo.jpg' . '" class="navbar-logo"></a>
-            	        </div>
-                    </div>
-                    <div class="col-sm-7">
-                        <h5 class="m-0">
-                        	<a href="../includes/product.php?itemid=' . $item['AdvertisementId'];
-                            if(isset($_SESSION['login']))
-                                echo '&login=1';
-                                echo '" class="text-info">' . $item['Summary'] . '</a></h5>';
-                                echo '<h4 class="m-0 price-color"><strong>' . $item['Price'] . ' Ft</strong></h4>';
-                                echo '<p class="mb-1 font-weight-light small">' . $item['PlaceOfReceipt'] . ' | ';
-                                echo $item['CreateDate'] . '</p>
-                                <h5 class="small"><a href="../includes/category.php';
-                                if(isset($_SESSION['login']))
-                                    echo '?login=1';
-                                    echo '" class="text-info"><strong>' . '#kategórianév' . '</strong></a></h5>
+    /*echo '  
+    		<div class="col-6 col-sm-4 col-lg-3">
+        		<div class="levanderBlushLighter rounded mb-1">
+            		<a href="../includes/product.php?itemid=' . $item['AdvertisementId'] . '" class="image-wrapper">
+                    <img class="img-fluid rounded image" src="' . '/uploadedImages/babacipo.jpg' . '" class="navbar-logo">
+                    </a>
+    	        </div>
+            </div>
+            <div class="col-6 col-sm-4 col-lg-3">
+                <h5 class="m-0">
+                	<a href="../includes/product.php?itemid=' . $item['AdvertisementId'] . '" class="text-info">' . $item['Summary'] . '</a></h5>
+                    <h4 class="m-0 price-color"><strong>' . $item['Price'] . ' Ft</strong></h4>
+                    <p class="mb-1 font-weight-light small">' . $item['PlaceOfReceipt'] . ' | ' . $item['CreateDate'] . '</p>
+                    <h5 class="small"><a href="../includes/category.php" class="text-info"><strong>' . '#kategórianév' . '</strong></a></h5>
+            </div>';*/
+    echo '  <div class="col-6 col-sm-4 col-lg-3 px-1">
+                <div class="card border-0 levanderBlushLighter mb-1">
+                    <a href="../includes/product.php?itemid=' . $item['AdvertisementId'] . '" class="image-wrapper">
+                    <img class="card-img-top img-fluid rounded image" src="' . '../uploadedImages/babacipo_portrait.jpg' . '" class="navbar-logo">
+                    </a>
+                    <div class="card-body p-1 levanderBlushLighter">
+                        <h5 class="m-0 mb-1"><a href="../includes/product.php?itemid=' . $item['AdvertisementId'] . '" class="text-info">' . $item['Summary'] . '</a></h5>
+                        <h4 class="m-0"><strong>' . $item['Price'] . ' Ft</strong></h4>
+                        <p class="mb-1 font-weight-light small">' . $item['PlaceOfReceipt'] . '</p>
+                        <p class="mb-1 font-weight-light small">' . $item['CreateDate'] . '</p>
+                        <h5 class="small"><a href="../includes/category.php" class="text-info"><strong>' . '#kategórianév' . '</strong></a></h5>
                     </div>
                 </div>
             </div>';
-}
+}       
+        
+echo '  </div>
+    </div>';
 
 include 'pagination.php';
 
